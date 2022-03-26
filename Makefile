198c5b4a1d13ea220eff8da67c3532ee84e3b232
Abi Ayad, Fethi Bey <abi_ayad.fethi_bey@courrier.uqam.ca>
2022-03-26 15:00:44 -0400

    implementation de la route GET /api/installations

--- Makefile
+++ Makefile
@@ -2,4 +2,5 @@ export FLASK_APP=app.py
 export FLASK_DEBUG=1
 
 run:
+	raml2html doc.raml > templates/doc.html
 	flask run
--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask import request
+from flask import render_template
+from flask import jsonify
+
 from planification import demarrer_planification
 from models import *
 from import_donnees import importer_donnees
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/sqlite.db'
@@ -12,11 +15,22 @@ db.init_app(app)
 
 @app.route('/doc')
 def documentation():
+    return render_template('doc.html')
 
-    return 'documentation'
+
+@app.route('/api/installations/')
+def liste_installations():
+    installations = None
+    if request.args.get('arrondissement'):
+        installations = Installation.query.filter_by(
+            arrondissement=request.args.get('arrondissement'))
+    else:
+        installations = Installation.query.all()
+
+    data = [{"nom": inst.nom, "type": inst.type} for inst in installations]
+    return jsonify(data)
 
 
 with app.app_context():
-    importer_donnees()
+    #importer_donnees()
     demarrer_planification()
-
