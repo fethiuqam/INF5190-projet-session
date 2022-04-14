a55f46ecb1a26e5bd1305ba9f23e8f8d86947333
Abi Ayad, Fethi Bey <abi_ayad.fethi_bey@courrier.uqam.ca>
2022-04-14 14:11:05 -0400

    mise a jour de db url

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
 
+
 class Config:
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class HerokuConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://lttrqunjwirccz:84a044558cb0d36c' \
+                              'ff8188a8b08694d8fd720c4541b98043820ca89a60b' \
+                              'b9443@ec2-54-80-123-146.compute-1.' \
+                              'amazonaws.com:5432/d6l61vp1vehd4k'
 
 
 class DevConfig(Config):
@@ -16,4 +20,3 @@ class DevConfig(Config):
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db/sqlite.db'
-
